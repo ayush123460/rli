d93c70def65d4844ab9618eba42dc6f177c7f7c4
Ayush Mukherjee <ayushm123460@gmail.com>
2019-01-18 09:38:12 +0530

    set up basic styles

--- server/resources/views/header.blade.php
+++ server/resources/views/header.blade.php
@@ -10,10 +10,21 @@
     <title>RL INDIA</title>
     <!-- SEO -->
     <meta name="author" content="Ayush Mukherjee">
-    <meta name="description" content="Internet home of the largest Rocket League community in Asia.">
+    <meta name="description" content="Internet home of the largest Rocket League&reg; community in Asia.">
+    {{-- Import CSS --}}
+    <link rel="stylesheet" type="text/css" href="@assets('css/main.css')">
 </head>
 <body>
-    <header></header>
+    <header>
+        {{-- Left Side Navigation --}}
+        <div class="left"></div>
+        {{-- Logo --}}
+        <div class="main"></div>
+        {{-- Right side Navigation --}}
+        <div class="right"></div>
+        {{-- Login/Logout button --}}
+        <div class="login"></div>
+    </header>
     <div class="container-fluid">
         @yield('content')
     </div>
