6e164a907c2337fd34328b455abc8cab76faf910
Ayush Mukherjee <ayushm123460@gmail.com>
2019-01-18 08:47:14 +0530

    setup laravel

---
+++ server/resources/views/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Set Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Set IE EDGE compatibility -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Set title -->
+    <title>RL INDIA</title>
+    <!-- SEO -->
+    <meta name="author" content="Ayush Mukherjee">
+    <meta name="description" content="Internet home of the largest Rocket League community in Asia.">
+</head>
+<body>
+    <header></header>
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+    <footer></footer>
+</body>
+</html>
