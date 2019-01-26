fe312258045b700ea6f6087242aaec1744cb6aa0
Ayush Mukherjee <ayushm123460@gmail.com>
2019-01-26 21:53:38 +0530

    home page additions

--- server/resources/views/pages/home.blade.php
+++ server/resources/views/pages/home.blade.php
@@ -18,4 +18,43 @@
     
 </section>
 
+<section class="tournaments">
+    <div class="container-fluid">
+        <div class="title">
+            <div class="bar"></div>
+            <h1>our latest</h1>
+        </div>
+
+        <div class="row">
+                <div class="card">
+                    <div class="card-title">APL 3.0 Qualifier 1</div>
+                    <div class="card-subtitle">APL eSports</div>
+                    <div class="card-img"></div>
+                    <div class="card-body">
+                        Date: 9 Feb 2019
+                    </div>
+                    <div class="card-cta">more info</div>
+                </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-title">Community Cup 8</div>
+                    <div class="card-subtitle">RL India</div>
+                    <div class="card-img"></div>
+                    <div class="card-body"> dummy text </div>
+                    <div class="card-cta">more info</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-title">Dummy title</div>
+                    <div class="card-subtitle">Dummy sub</div>
+                    <div class="card-img"></div>
+                    <div class="card-body"> dummy text </div>
+                    <div class="card-cta">more info</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
